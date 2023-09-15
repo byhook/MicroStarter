fb1d8d529222f6fa8f3b19ef63e5fa575a64648c
byhook <byhook@163.com>
2023-09-15 21:39:19 +0800

    初步加入拖拽文件保存配置的逻辑

---
+++ MicroStarter/IconManager.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinStarter
+{
+    public sealed class IconManager
+    {
+
+        [DllImport("shell32.dll")]
+        static extern IntPtr SHGetFileInfo(string pszPath, uint dwFileAttributes, ref SHFILEINFO psfi, uint cbSizeFileInfo, uint uFlags);
+        struct SHFILEINFO
+        {
+            public IntPtr hIcon;
+            public int iIcon;
+            public uint dwAttributes;
+            public char szDisplayName;
+            public char szTypeName;
+        }
+
+        private static readonly Lazy<IconManager> _lazy =
+            new Lazy<IconManager>(() => new IconManager());
+
+        private IconManager() { }
+
+        public static IconManager GetInstance()
+        {
+            return _lazy.Value;
+        }
+
+
+        public Icon getTargetIcon(String execPath)
+        {
+            const uint SHGFI_ICON = 0x000000100;     // Get icon
+            const uint SHGFI_LARGEICON = 0x000000000;  // Get large icon
+            const uint SHGFI_SMALLICON = 0x000000001;  // Get small icon   
+
+            SHFILEINFO fi = new SHFILEINFO();
+            Icon ic = null;
+            //SHGFI_ICON + SHGFI_USEFILEATTRIBUTES + SmallIcon   
+            int iTotal = (int)SHGetFileInfo(execPath, 0, ref fi, (uint)Marshal.SizeOf(fi),
+                SHGFI_ICON | SHGFI_LARGEICON);
+            if (iTotal > 0)
+            {
+                ic = Icon.FromHandle(fi.hIcon);
+            }
+
+            /*
+            string filePath = @"I:\\MapleStory\\冒险岛客户端\\冒险岛079\\MapleStarter.exe";
+            Icon ic = Icon.ExtractAssociatedIcon(filePath);
+            */
+            return ic;
+        }
+
+    }
+}
--- MicroStarter/MainForm.cs
+++ MicroStarter/MainForm.cs
@@ -1,4 +1,7 @@
+using System.IO;
+using System.Windows.Forms;
 using WinStarter;
+using static System.Windows.Forms.LinkLabel;
 
 namespace MicroStarter
 {
@@ -21,13 +24,55 @@ namespace MicroStarter
                 tabListView.Dock = DockStyle.Fill;
                 mainPage.Controls.Add(tabListView);
 
+                ImageList imageList = new ImageList();
+                imageList.ImageSize = new Size(48,48);
+                imageList.ColorDepth = ColorDepth.Depth32Bit;
+            
+                tabListView.View = View.LargeIcon;
+                tabListView.LargeImageList = imageList;
+
+                if(!(tabData.TabItemDatas is null))
+                {
+                    tabListView.BeginUpdate();
+                    foreach (TabItemData tabItemData in tabData.TabItemDatas)
+                    {
+                        Icon icon = IconManager.GetInstance().getTargetIcon(tabItemData.ItemPath);
+                        imageList.Images.Add(icon);
+                        ListViewItem item = new ListViewItem(tabItemData.ItemName);
+                        item.ImageIndex = tabListView.Items.Count;
+                        tabListView.Items.Add(item);
+                    }
+                    tabListView.EndUpdate();
+                }
+
                 mainTabControl.TabPages.Add(mainPage);
             }
         }
 
+        public static readonly Guid CLSID_WSH_SHELL = new Guid("72C24DD5-D70A-438B-8A42-98424B88AFB8");
         private void MainForm_DragDrop(object sender, DragEventArgs e)
         {
-
+            string[] dropFiles = (string[])e.Data.GetData(DataFormats.FileDrop);
+            // 对拖放的文件进行处理
+            foreach (string filePath in dropFiles)
+            {
+                var tabItemData = new TabItemData();
+                if (Path.GetExtension(filePath) == ".lnk")
+                {
+                    dynamic objWshShell = Activator.CreateInstance(Type.GetTypeFromCLSID(CLSID_WSH_SHELL));
+                    var objShortcut = objWshShell.CreateShortcut(filePath);
+                    tabItemData.ItemPath = objShortcut.TargetPath;
+                    string fileName = Path.GetFileName(objShortcut.TargetPath);
+                    tabItemData.ItemName = fileName;
+                } else
+                {
+                    string fileName = Path.GetFileName(filePath);
+                    tabItemData.ItemName = fileName;
+                    tabItemData.ItemPath = filePath;
+                }
+                ConfigManager.GetInstance().AddTabItemData(mainTabControl.SelectedIndex, tabItemData);
+            }
+            ConfigManager.GetInstance().SaveConfig();
         }
 
         private void MainForm_DragEnter(object sender, DragEventArgs e)
